df8fd987611f3fce8b839ee9bb9250d1e5521020
Jayala2023 <jayalapadilla@student.neumont.edu>
2023-08-08 19:41:23 -0600

    Commit test

--- src/main/java/pro100/ayala/juan/Main.java
+++ src/main/java/pro100/ayala/juan/Main.java
@@ -2,6 +2,6 @@ package pro100.ayala.juan;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Controller database = new Controller();
     }
 }
